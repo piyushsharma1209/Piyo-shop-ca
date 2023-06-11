20f47d39d80d7c0c562676a94cd7cd4fdf5cc841
piyushsharma1209 <piyush.sharma@stud.noroff.no>
2023-06-11 14:58:54 +0200

    Update footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import styles from './Footer.module.css';
 
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <div className={styles.quickLinks}>
-                <Link to="/" className={styles.link}>
-                    Home
-                </Link>
-                <Link to="/contact" className={styles.link}>
-                    Contact
-                </Link>
-                <Link to="/about" className={styles.link}>
-                    About
-                </Link>
-            </div>
             <div className={styles.copyright}>
-                &copy; {new Date().getFullYear()} - Piyu Co
+                &copy; {new Date().getFullYear()} - Piyo&Co
             </div>
         </footer>
     );
